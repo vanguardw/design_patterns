a4009ffba8e8eb8b2a780ba586c9841f1de510e9
vanguard <vanguardd@outlook.com>
2018-12-13 17:31:34 +0800

    完成策略模式

---
+++ strategy/src/com/vangaurd/Main.java
@@ -0,0 +1,23 @@
+package com.vangaurd;
+
+/**
+ * @Title:
+ * @Description:
+ * @Author: vanguard
+ * @Version: 1.0
+ * @Date: 2018/12/13
+ */
+public class Main {
+    public static void main(String[] args) {
+        double totalPrice = 500;
+        //工厂模式
+        /*CashSuper cashSuper = CashFactory.createCashAccept("打8折");
+        double payAmount = cashSuper.acceptCash(totalPrice);
+        System.out.println(payAmount);*/
+
+        //策略模式和工厂模式结合
+        CashContext cashContext = new CashContext("满300减100");
+        double payAmount = cashContext.getResult(totalPrice);
+        System.out.println(payAmount);
+    }
+}
