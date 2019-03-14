15079eef8947f990c9271340276e1f3caa81721c
vanguard <wang1141716638@outlook.com>
2019-03-14 21:53:23 +0800

    增加装饰着模式案例

---
+++ decorator/src/com/vanguard/BigTrouser.java
@@ -0,0 +1,17 @@
+package com.vanguard;
+
+/**
+ * @Title: 垮裤具体服饰类
+ * @Description: ConcreteDecorator
+ * @Author: vanguard
+ * @Version: 1.0
+ * @Date: 2019/03/14
+ */
+public class BigTrouser extends Finery {
+
+    @Override
+    public void show() {
+        System.out.print("垮裤  ");
+        super.show();
+    }
+}
